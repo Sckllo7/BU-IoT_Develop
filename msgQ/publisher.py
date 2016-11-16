4a3611497de286373fe3a23a1ac7bc68fcf4f36a
Junsu Kim <Tomy2174@gmail.com>
2016-11-16 15:28:17 +0900

    Anotations are removed

--- msgQ/publisher.py
+++ msgQ/publisher.py
@@ -36,28 +36,3 @@ while (1) :
 
     mqttc.disconnect()
     time.sleep(random.randrange(1, 3))
-
-"""
-while (1) :
-        humidity, temperature = Adafruit_DHT.read_retry(11, 4)
-
-        temp = { 'hostname' : hostname,
-                 'name' : 'Temperature',
-                 'value' : temperature }
-
-        humi = { 'hostname' : hostname,
-                 'name' : 'Humidity',
-                 'value' : humidity }
-
-        jsonTemp = json.dumps(temp)
-        jsonHumi = json.dumps(humi)
-         
-        if humidity is not None and temperature is not None:
-                mqttc.publish("Home/LvRoom/Temp", jsonTemp)
-                mqttc.publish("Home/LvRoom/Humi", jsonHumi)
-
-        else:
-                print 'Failed to get reading. Try again!'
-
-        time.sleep(1)
-"""

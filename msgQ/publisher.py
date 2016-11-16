a8dde383ba966aae19adb4d642c833ae056a00bf
sckllo7 <tomy2174@gmail.com>
2016-11-16 15:24:06 +0900

    loop is added

--- msgQ/publisher.py
+++ msgQ/publisher.py
@@ -6,33 +6,37 @@ import Adafruit_DHT
 import paho.mqtt.client as mqtt
 import time
 import os
+import random
+
+hostname = os.uname()[1]
 
 mqttc = mqtt.Client("python_pub")
 mqttc.connect("buhadoop", 1883)
 
-hostname = os.uname()[1]
+while (1) :
+    humidity, temperature = Adafruit_DHT.read_retry(11, 4)
 
-humidity, temperature = Adafruit_DHT.read_retry(11, 4)
+    temp = { 'hostname' : hostname,
+             'name' : 'Temperature',
+             'value' : temperature }
 
-temp = { 'hostname' : hostname,
-         'name' : 'Temperature',
-         'value' : temperature }
+    humi = { 'hostname' : hostname,
+             'name' : 'Humidity',
+             'value' : humidity }
 
-humi = { 'hostname' : hostname,
-         'name' : 'Humidity',
-         'value' : humidity }
+    jsonTemp = json.dumps(temp)
+    jsonHumi = json.dumps(humi)
 
-jsonTemp = json.dumps(temp)
-jsonHumi = json.dumps(humi)
- 
-if humidity is not None and temperature is not None:
-        mqttc.publish("Home/LvRoom/Temp", jsonTemp)
-        mqttc.publish("Home/LvRoom/Humi", jsonHumi)
+    if humidity is not None and temperature is not None:
+#            mqttc.publish("Home/LvRoom/Temp", jsonTemp)
+            mqttc.publish("Home/LvRoom/Humi", jsonHumi)
 
-else:
-        print 'Failed to get reading. Try again!'
+    else:
+            print 'Failed to get reading. Try again!'
 
-time.sleep(1)
+    mqttc.disconnect()
+    time.sleep(random.randrange(1, 3))
+
 """
 while (1) :
         humidity, temperature = Adafruit_DHT.read_retry(11, 4)

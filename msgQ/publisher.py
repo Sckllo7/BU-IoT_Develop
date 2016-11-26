8c6555633ebe16deca75732b2ea28a177e35bd33
Sckllo7 <tomy2174@gmail.com>
2016-11-26 21:45:10 +0900

    60sec test version

--- msgQ/publisher.py
+++ msgQ/publisher.py
@@ -9,8 +9,13 @@ import os
 import random
 
 hostname = os.uname()[1]
+pHumidity = 0
+pTemperature = 0
+stamp = 0.0
 
 while (1) :
+    time.sleep(5)
+
     humidity, temperature = Adafruit_DHT.read_retry(11, 4)
 
     temp = { 'hostname' : hostname,
@@ -24,15 +29,30 @@ while (1) :
     jsonTemp = json.dumps(temp)
     jsonHumi = json.dumps(humi)
 
+    if (humidity != pHumidity & temperature != pTemperature) | (time.time() - stamp) > 60 :
+	swit = 3
+    elif humidity != pHumidity & temperature == pTemperature :
+	swit = 2
+    elif humidity == pHumidity & temperature != pTemperature :
+	swit = 1
+    else
+	continue;
+
+    stamp = time.time()
+
     mqttc = mqtt.Client("python_pub")
     mqttc.connect("buhadoop", 1883)
 
     if humidity is not None and temperature is not None:
-            mqttc.publish("Home/Univer/Temp", jsonTemp)
-            mqttc.publish("Home/Univer/Humi", jsonHumi)
+	    if swit == 3 :
+		    mqttc.publish("Home/Univer/Temp", jsonTemp)
+		    mqttc.publish("Home/Univer/Humi", jsonHumi)
+	    elif swit == 2:
+		    mqttc.publish("Home/Univer/Temp", jsonTemp)
+	    elif swit == 1:
+		    mqttc.publish("Home/Univer/Humi", jsonHumi)
 
     else:
             print 'Failed to get reading. Try again!'
 
     mqttc.disconnect()
-    time.sleep(5)
